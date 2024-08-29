26cbf15294e0037ed7b0069e50bdf4a6c89f359d
David Alder <david.alder11@gmail.com>
2024-08-29 16:19:56 -0400

    set up chakra

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { ChakraBaseProvider } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ChakraBaseProvider>
+    <ChakraProvider>
       <App />
-    </ChakraBaseProvider>
+    </ChakraProvider>
   </StrictMode>,
 )
